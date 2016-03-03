8e164651174f0613d76b2b93a05adb39931ecf7f
James Valaitis <james@infinityjames.com>
2016-03-03 11:17:24 +0000

    Created a cell to represent a comic book in a collection view.

---
+++ MarvelWow/View/ComicBookCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ComicBookCollectionViewCell.swift
+//  MarvelWow
+//
+//  Created by James Valaitis on 03/03/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import UIKit
+
+//	MARK: Comics Book Collection View Cell
+
+/**
+    `ComicBookCollectionViewCell`
+
+    A cell used to display a comic book within a collection view.
+ */
+class ComicBookCollectionViewCell: UICollectionViewCell {
+    
+}
