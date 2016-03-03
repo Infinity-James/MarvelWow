655d6cd858ac0ee33b82d067492faf1c71001873
James Valaitis <james@infinityjames.com>
2016-03-03 10:26:36 +0000

    Created a function on the RemoteResource protocol that gives default behaviour for loading the resource.

--- MarvelWow/Model/JSONResource.swift
+++ MarvelWow/Model/JSONResource.swift
@@ -18,3 +18,43 @@ import Foundation
 */
 protocol RemoteResource {
 }
+
+/// A closure called upon the completion of the loading of a remote resource.
+typealias RemoteResourceHandler = (data: NSData?, success: Bool) -> ()
+
+extension RemoteResource {
+    
+    /**
+        Loads the data defined by this resource.
+     
+        - Parameter URLPath:    The path at which the data defined by this remote resource resides.
+        - Parameter completion: A closure which will be executed upon completion the load (successful or otherwise).
+     */
+    mutating func load(URLPath: String, completion: RemoteResourceHandler) {
+        
+        //  cannot load an invalid URL
+        guard let URL = NSURL(string: URLPath) else { fatalError("Invalid URL for resource: \(URLPath)") }
+        
+        //  create a task to download the resource
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(URL) { data, response, error in
+            //  if we didn't get the data or there was an issue we fail out
+            guard let HTTPResponse = response as? NSHTTPURLResponse, let data = data where error == nil else {
+                print("Loading the remote resource failed.\n")
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                completion(data: nil, success: false)
+                return
+            }
+            
+            //  if we get here, everything worked
+            print("Response Code: \(HTTPResponse.statusCode)")
+            
+            completion(data: data, success: true)
+        }
+        
+        //  start the download
+        task.resume()
+    }
+}
