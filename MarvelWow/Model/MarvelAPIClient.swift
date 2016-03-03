f4c7f4b5c467c8f36ac837998dadeabc3cfc1209
James Valaitis <james@infinityjames.com>
2016-03-03 11:32:03 +0000

    Created a class with the intention of being our native client to Marvel's API.

---
+++ MarvelWow/Model/MarvelAPIClient.swift
@@ -0,0 +1,20 @@
+//
+//  MarvelAPIClient.swift
+//  MarvelWow
+//
+//  Created by James Valaitis on 03/03/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import Foundation
+
+//	MARK: Marvel API Client
+
+/**
+    `MarvelAPIClient`
+
+    A native Swift client for querying Marvel's API.
+ */
+class MarvelAPIClient {
+    
+}
