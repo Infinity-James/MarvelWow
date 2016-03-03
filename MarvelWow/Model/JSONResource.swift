c454ee522bdcfeb3ba9d9878b6437a9825beabdc
James Valaitis <james@infinityjames.com>
2016-03-03 10:24:26 +0000

    Stubbed out a protocol defining a RemoteResource.

---
+++ MarvelWow/Model/JSONResource.swift
@@ -0,0 +1,20 @@
+//
+//  JSONResource.swift
+//  MarvelWow
+//
+//  Created by James Valaitis on 02/03/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import Foundation
+
+//	MARK: Remote Resource
+
+/**
+    `RemoteResource`
+
+    A protocol that defines a remotely obtained resource.
+    Objects conforming to this protocol can be loaded.
+*/
+protocol RemoteResource {
+}
