f0132cd13763df7ff9fba8c27d10198132749a68
James Valaitis <james@infinityjames.com>
2016-03-03 11:13:33 +0000

    Set up the comics collection view controller in the storyboard.

---
+++ MarvelWow/Controller/ComicsCollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ComicsCollectionViewController.swift
+//  MarvelWow
+//
+//  Created by James Valaitis on 03/03/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import UIKit
+
+//	MARK: Comics Collection View Controller
+
+/**
+    `ComicsCollectionViewController`
+
+    A controller of a collection view used to display comic books, ordered by default from newest to oldest.
+ */
+class ComicsCollectionViewController: UICollectionViewController {
+}
