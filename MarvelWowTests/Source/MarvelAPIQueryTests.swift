b80ea98f260769dca0ab30e59e2af6dce708f8ff
James Valaitis <james@infinityjames.com>
2016-03-03 18:47:01 +0000

    Created a file with the intention to test the MarvelAPIQuery types.

---
+++ MarvelWowTests/Source/MarvelAPIQueryTests.swift
@@ -0,0 +1,20 @@
+//
+//  MarvelAPIQueryTests.swift
+//  MarvelWow
+//
+//  Created by James Valaitis on 03/03/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import XCTest
+@testable import MarvelWow
+
+//	MARK: Marvel API Query Tests Class
+
+/**
+    `MarvelAPIQueryTests`
+
+    A class that will test the various elements of the Marvel API Query structure.
+ */
+class MarvelAPIQueryTests: XCTestCase {
+}
