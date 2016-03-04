594a784af7ac1247932ffa854d36216053122458
James Valaitis <james@infinityjames.com>
2016-03-04 10:19:26 +0000

    Added a tests class for the Marvel API Client.

---
+++ MarvelWowTests/Source/MarvelAPIClientTests.swift
@@ -0,0 +1,21 @@
+//
+//  MarvelAPIClientTests.swift
+//  MarvelWow
+//
+//  Created by James Valaitis on 04/03/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import XCTest
+@testable import MarvelWow
+
+//	MARK: Marvel API Client Tests Class
+
+/**
+    `MarvelAPIClientTests`
+
+    Tests the Marvel API Client's ability to communicate correctly with the Marvel servers.
+ */
+class MarvelAPIClientTests: XCTestCase {
+    
+}
