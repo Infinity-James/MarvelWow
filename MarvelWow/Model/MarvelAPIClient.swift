f635afbb9aded5f9586b908a0c77d2a121bc79ef
James Valaitis <james@infinityjames.com>
2016-03-04 20:26:07 +0000

    Stubbed out the idea of an authorized query request.

--- MarvelWow/Model/MarvelAPIClient.swift
+++ MarvelWow/Model/MarvelAPIClient.swift
@@ -21,21 +21,82 @@ class MarvelAPIClient: NSObject {
     
     /// The base of API endpoint.
     private let baseEndpointURL = NSURL(string:"https://gateway.marvel.com/")!
-    private let URLSession: NSURLSession = {
-        let session = NSURLSession()
-        return session
+    
+    private let APIParameterKey = "apikey"
+    private let hashParameterKey = "hash"
+    private let timestampParameterKey = "ts"
+    
+    private let publicAPIKey = "1fad7c8c49179fc7287b7a8d635a0483"
+    
+    //	MARK: Marvel API Client Errors
+    enum MarvelAPIClientError: ErrorType {
+        /// The query was previously made and is still in process.
+        case QueryExists
+    }
+    
+    //	MARK: Properties
+    
+    /// The URL session used to create our data tasks. Ideally this would be a constant (and would not be an implicitly unwrapped optional).
+    /// However the initializer requires 'self' (because we want to be the delegate).
+    private var session: NSURLSession!
+    private let sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+    private let sessionQueue: NSOperationQueue = {
+        let sessionQueue = NSOperationQueue()
+        sessionQueue.name = "Marvel API Session Queue"
+        return sessionQueue
     }()
+    
+    
     
     //	MARK: Type Alias
     
     /// A closure that can be called when a query has received a response
     typealias QueryCompleted = () -> ()
     
+    //	MARK: Initialization
+    
+    override init() {
+        
+        super.init()
+        
+        session = NSURLSession(configuration: sessionConfiguration, delegate: self, delegateQueue: sessionQueue)
+    }
+    
     //	MARK: API Communication
     
+    /**
+        Accepts a query object and fires it at Marvel's super awesome API.
+    
+        - Parameter query:      The query that Marvel need to handle (Captain America could probably get involved if it proves too difficult).
+        - Parameter completion: Once Marvel answer our query (and they will because the good guys always win), this completion handler will be called.
+     */
     func executeQuery(query: MarvelAPIQuery, completion: QueryCompleted) {
         let endpointURL = baseEndpointURL.URLByAppendingPathComponent(query.endpointAPIPath)
         let fullURL = endpointURL.URLByAppendingPathComponent(query.fullQueryPathComponent)
+        let URLRequest = authorizedURLRequest(fromURL: fullURL)
+        let dataTask = session.dataTaskWithRequest(URLRequest)
+        dataTask.resume()
+    }
+    
+    //	MARK: Authorization
+    
+    private func authorizedURLRequest(fromURL URL: NSURL) -> NSURLRequest {
+        
+        let request = NSMutableURLRequest(URL: URL)
+        //  all requests to Marvel are "GET"
+        request.HTTPMethod = "GET"
+        //  set a sensible timeout
+        request.timeoutInterval = 20.0
+        
+        let parameters = [APIParameterKey: publicAPIKey]
+        do {
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(parameters, options: .PrettyPrinted)
+        } catch {
+            fatalError("Setting the HTTP Body for authorization is not working: \(parameters)\nError: \(error)")
+        }
+        
+        return request.copy() as! NSURLRequest
+        
     }
 }
 
