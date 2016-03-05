e1c998e33fa67996c6f50544559cfc71f8e71121
James Valaitis <james@infinityjames.com>
2016-03-05 18:24:01 +0000

    Added a bridging header with the sole purporse of import CommonCrypto to allow for an extension on String that creates an MD5 hash of the string.

---
+++ MarvelWow/Supporting-Files/MarvelWow-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  MarvelWow-Bridging-Header.h
+//  MarvelWow
+//
+//  Created by James Valaitis on 05/03/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+#ifndef MarvelWow_Bridging_Header_h
+#define MarvelWow_Bridging_Header_h
+
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* MarvelWow_Bridging_Header_h */
