73dce5e7cb947232fb6a3249bbd3ef1f40a1ebcf
James Valaitis <james@infinityjames.com>
2016-03-05 18:29:05 +0000

    Fixed the client according to the problems encountered by my handy dandy test case.

--- MarvelWow/Model/MarvelAPIClient.swift
+++ MarvelWow/Model/MarvelAPIClient.swift
@@ -20,18 +20,22 @@ class MarvelAPIClient: NSObject {
     //	MARK: Constants
     
     /// The base of API endpoint.
-    private let baseEndpointURL = NSURL(string:"https://gateway.marvel.com/")!
+    private let baseEndpoint = "https://gateway.marvel.com"
     
     private let APIParameterKey = "apikey"
     private let hashParameterKey = "hash"
     private let timestampParameterKey = "ts"
     
     private let publicAPIKey = "1fad7c8c49179fc7287b7a8d635a0483"
+    private let privateAPIKey = "c686eb280010eaa2519c662752304ff50c3ef145"
     
     //	MARK: Marvel API Client Errors
+    
     enum MarvelAPIClientError: ErrorType {
         /// The received JSON for the query was in an unexpected format.
         case InvalidJSON
+        /// The URL for the query is not valid.
+        case InvalidURL
         /// The query was previously made and is still in process.
         case QueryExists
     }
@@ -51,6 +55,16 @@ class MarvelAPIClient: NSObject {
     private var receivedData = [NSURL: NSMutableData]()
     /// A store for an query completion closures mapping the URL of the query to the closure.
     private var queryCompletionClosures = [NSURL: QueryCompleted]()
+    /// Parameters that need to be added to a URL request to the Marvel servers.
+    private var APIParameters: [String: String] {
+        let timestamp = NSDate.timeIntervalSinceReferenceDate()
+        let hashBase = String(timestamp) + privateAPIKey + publicAPIKey
+        let parameters = [APIParameterKey: publicAPIKey,
+            timestampParameterKey: String(timestamp),
+            hashParameterKey: hashBase.md5]
+        
+        return parameters
+    }
     
     
     
@@ -82,20 +96,29 @@ class MarvelAPIClient: NSObject {
         - Parameter completion: Once Marvel answer our query (and they will because the good guys always win), this completion handler will be called.
      */
     func executeQuery(query: MarvelAPIQuery, completion: QueryCompleted) throws {
+        
         //  create the URL for the API request
-        let endpointURL = baseEndpointURL.URLByAppendingPathComponent(query.endpointAPIPath)
-        let fullURL = endpointURL.URLByAppendingPathComponent(query.fullQueryPathComponent)
+        let endpoint = baseEndpoint + query.endpointAPIPath + "?" + query.fullQueryPathComponent
+        guard let endpointURL = NSURL(string: endpoint) else {
+            throw MarvelAPIClientError.InvalidURL
+        }
+        
+        //  create the request up top to get the URL we will use as the key
+        let URLRequest = authorizedURLRequest(fromURL: endpointURL)
+        guard let finalURL = URLRequest.URL else {
+            throw MarvelAPIClientError.InvalidURL
+        }
         
         //  if this query is already in process we throw the appropriate error, otherwise we store the completion closure
-        if let _ = queryCompletionClosures[fullURL] {
+        if let _ = queryCompletionClosures[finalURL] {
             throw MarvelAPIClientError.QueryExists
         } else {
-            queryCompletionClosures[fullURL] = completion
-            receivedData[fullURL] = NSMutableData()
+            queryCompletionClosures[finalURL] = completion
+            receivedData[finalURL] = NSMutableData()
         }
         
         //  create the request and data task and then start it
-        let URLRequest = authorizedURLRequest(fromURL: fullURL)
+        
         let dataTask = session.dataTaskWithRequest(URLRequest)
         dataTask.resume()
     }
@@ -129,18 +152,19 @@ class MarvelAPIClient: NSObject {
     
     private func authorizedURLRequest(fromURL URL: NSURL) -> NSURLRequest {
         
-        let request = NSMutableURLRequest(URL: URL)
+        //  create authenticating URL component and append it to the existing query
+        let components = NSURLComponents(URL: URL, resolvingAgainstBaseURL: false)!
+        var authenticationComponentString = ""
+        for (key, value) in APIParameters {
+            authenticationComponentString += "\(key)=\(value)&"
+        }
+        components.query = (components.query ?? "") + "&" + authenticationComponentString
+        
+        let request = NSMutableURLRequest(URL: components.URL!)
         //  all requests to Marvel are "GET"
         request.HTTPMethod = "GET"
         //  set a sensible timeout
         request.timeoutInterval = 20.0
-        
-        let parameters = [APIParameterKey: publicAPIKey]
-        do {
-            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(parameters, options: .PrettyPrinted)
-        } catch {
-            fatalError("Setting the HTTP Body for authorization is not working: \(parameters)\nError: \(error)")
-        }
         
         return request.copy() as! NSURLRequest
         
@@ -163,12 +187,22 @@ extension MarvelAPIClient: NSURLSessionDataDelegate {
         receivedData[URL] = currentData
     }
     
+    
+    
     func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+        
+        if let error = error {
+            print("Error occured whilst executing data task: \(error.localizedDescription)\nCode: \(error.code)\nReason: \(error.localizedFailureReason)")
+            print("Original Request: \(task.originalRequest)")
+            print("Response: \(task.response)")
+            return
+        }
+        
         //  we should only be dealing with data tasks, the task should have an associated URL, and that URL must map to a closure and received data
         guard let dataTask = task as? NSURLSessionDataTask,
             URL = dataTask.originalRequest?.URL,
             queryCompleted = queryCompletionClosures[URL],
-            data = receivedData[URL]?.copy() as? NSData where error == nil else {
+            data = receivedData[URL]?.copy() as? NSData else {
                 print("Error occured when executing the NSURLSessionTask (\(task)): \(error)")
                 return
         }
@@ -195,5 +229,34 @@ extension MarvelAPIClient: NSURLSessionDataDelegate {
         
         //  everything went well
         queryCompleted(JSON: resultsJSON, error: nil)
+    }
+}
+
+//	MARK: Cryptography
+
+/**
+    This extension allows for an easy way to hash a string using md5.
+ */
+extension String  {
+    var md5: String {
+        //  create the required objects for hashing the string
+        let string = self.cStringUsingEncoding(NSUTF8StringEncoding)
+        let stringLength = CC_LONG(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+        let digestLength = Int(CC_MD5_DIGEST_LENGTH)
+        let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLength)
+        
+        //  hash the string passing our result pointer by reference
+        CC_MD5(string!, stringLength, result)
+        
+        //  create a string from the resulting hash
+        let hash = NSMutableString()
+        for i in 0..<digestLength {
+            hash.appendFormat("%02x", result[i])
+        }
+        
+        //  deallocate the pointer
+        result.dealloc(digestLength)
+        
+        return String(hash)
     }
 }
