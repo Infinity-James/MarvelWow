c9ce0b0bcb6a3a0ac54882d68c5a24bba3a25417
James Valaitis <james@infinityjames.com>
2016-03-05 14:59:09 +0000

    Added a struct which defines a Marvel comic resource.

--- MarvelWow/Model/MarvelComic.swift
+++ MarvelWow/Model/MarvelComic.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+//	MARK: Marvel Comics Struct
+
+/**
+    `MarvelComic`
+
+    A resource from the Marvel API that represents a comic book.
+ */
+struct MarvelComic: MarvelAPIResource {
+    
+    //	MARK: Properties - Resource
+    
+    /// The unique identifier for this comic.
+    let ID: Identifier
+    /// The canonical URL identifier for this comic.
+    let resourceURI: NSURL
+    
+    //	MARK: Properties - Comic
+    
+    /// The title of the comic.
+    let title: String
+    
+    //	MARK: Initialization
+    
+    init?(JSON: JSONValue) {
+        
+        //  a comic book cannot exist without an ID or resource URI
+        guard let identifier = JSON[MarvelAPIResourceIDKey] as? Identifier, URIString = JSON[MarvelAPIResourceURIKey] as? Identifier, URI = NSURL(string: URIString) else {
+            print("Expected a valid identifier and resource URI in JSON: \(JSON)")
+            return nil
+        }
+        
+        ID = identifier
+        resourceURI = URI
+        
+        //  pull the comic specific properties
+        guard let comicTitle = JSON["title"] as? String else {
+            print("What is a comic without a title? Title missing in JSON: \(JSON)")
+            return nil
+        }
+        
+        title = comicTitle
+    }
+}
