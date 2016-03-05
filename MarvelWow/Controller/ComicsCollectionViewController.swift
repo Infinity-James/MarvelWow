98ae173999a7589495554d630530117e44b36b5d
James Valaitis <james@infinityjames.com>
2016-03-05 19:40:07 +0000

    Started implementation of the comic collection view controller.

--- MarvelWow/Controller/ComicsCollectionViewController.swift
+++ MarvelWow/Controller/ComicsCollectionViewController.swift
@@ -16,4 +16,28 @@ import UIKit
     A controller of a collection view used to display comic books, ordered by default from newest to oldest.
  */
 class ComicsCollectionViewController: UICollectionViewController {
+    
+    //	MARK: Constants
+    
+    /// The number of comics we will fetch each time.
+    private let comicBatchSize = 60
+    
+    //	MARK: Properties - State
+    
+    /// The comics to display in the collection view.
+    private var comics = [MarvelComic]()
+    /// The client we will use to fetch the comics.
+    private let marvelAPIClient = MarvelAPIClient()
+    
+    //	MARK: Comic Fetching
+    
+    
+    
+    //	MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //  fetch the first set of comics
+    }
 }
