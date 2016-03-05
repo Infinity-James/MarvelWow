c0780153ee7de857a379f743ed2d2aa270767f69
James Valaitis <james@infinityjames.com>
2016-03-05 12:37:08 +0000

    Laid out foundation for a protocol that will define a resource returned by the Marvel API.

---
+++ MarvelWow/Model/MarvelAPIResource.swift
@@ -0,0 +1,20 @@
+//
+//  MarvelAPIResource.swift
+//  MarvelWow
+//
+//  Created by James Valaitis on 05/03/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import Foundation
+
+//	MARK: Marvel API Resource Protocol
+
+/**
+    `MarvelAPIResource`
+
+    Defines an object that can be returned by the Marvel API.
+ */
+protocol MarvelAPIResource {
+    
+}
