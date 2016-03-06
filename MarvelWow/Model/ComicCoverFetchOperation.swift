80f66f032101873249b039fb54f9dd1a8de0c228
James Valaitis <james@infinityjames.com>
2016-03-06 18:00:09 +0000

    Implemented the various methods available to us to fetch a comic cover image in the operation.

--- MarvelWow/Model/ComicCoverFetchOperation.swift
+++ MarvelWow/Model/ComicCoverFetchOperation.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 &Beyond. All rights reserved.
 //
 
+import SwiftyDropbox
 import UIKit
 
 //	MARK: Comic Cover Fetch Operation
@@ -53,28 +54,108 @@ class ComicCoverFetchOperation: NSOperation {
         super.init()
     }
     
+    //	MARK: Image Location Checking
+    
+    /**
+        Fetches the cover image from the Dropbox folder for this app.
+    
+        - Parameter client:     The Dropbox client to use to fetch the image from the server if it exists.
+        - Parameter completion: A block called once we have finished trying to fetch the image (successfully or not).
+            - coverImage:   The image if we managed to fetch it, `nil` if we failed.
+     */
+    private func fetchFromDropbox(withClient client: DropboxClient, completion: (coverImage: UIImage?) -> ()) {
+        let expectedFileName = String(comic.ID) + ".jpg"
+        
+        //  given a URL and a response return a URL to save the downloaded Dropbox image to
+        let destination : (NSURL, NSHTTPURLResponse) -> NSURL = { temporaryURL, response in
+            let fileManager = NSFileManager.defaultManager()
+            let directoryURL = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+            //  generate a unique name for this file in case we've seen it before
+            let UUID = NSUUID().UUIDString
+            let pathComponent = "\(UUID)-\(response.suggestedFilename!)"
+            return directoryURL.URLByAppendingPathComponent(pathComponent)
+        }
+        
+        //  if it exists in their Dropbox we download it
+        client.files.listFolder(path: "").response { response, error in
+            if let files = response where files.entries.contains( { $0.name == expectedFileName } ) {
+                client.files.download(path: expectedFileName, destination: destination).response { response, error in
+                    if let (_, URL) = response,
+                        imageData = NSData(contentsOfURL: URL),
+                        image = UIImage(data: imageData) {
+                            completion(coverImage: image)
+                    } else {
+                        completion(coverImage: nil)
+                    }
+                }
+            } else {
+                completion(coverImage: nil)
+            }
+        }
+    }
+    
+    /**
+        Fetches the cover image from Marvel's servers.
+     */
+    private func fetchFromServer() {
+        //  finally we fall back to fetching the image from it's location on the Marvel servers
+        guard let imageData = NSData(contentsOfURL: self.comicCoverImageURL),
+            image = UIImage(data: imageData) else {
+                print("Completely failed to find an image for the comic: \(self.comic)")
+                self.cancel()
+                return
+        }
+        
+        //  once we've fetched the image from the server we store it and cache it
+        self.coverImage = image
+        ComicCoverImageCache.cacheCoverImageData(imageData, forComic: self.comic)
+    }
+    
+    /**
+        Retrieves the cover image from teh cache if it is in there.
+        
+        - Returns:  `true` if it was found in the cache and retrieved, `false` otherwise.
+     */
+    private func retrieveFromCache() -> Bool {
+        //  first we check the cache for the cover image to save from fetching it
+        guard let cachedImage = ComicCoverImageCache.coverImageForComic(comic) else {
+            return false
+        }
+        
+        coverImage = cachedImage
+        return true
+    }
+    
     //	MARK: Operation
     
     override func main() {
         
-        let data: NSData?
-        
-        //  first we check the cache for the cover image to save from fetching it
-        
-        //  if it's not in the cache we check if the user has an image in their Dropbox for this comic
-        
-        //  finally we fall back to fetching the image from it's location on the Marvel servers
-        
-        data = NSData(contentsOfURL: comicCoverImageURL)
-        
-        //  we should have some valid data by now, but if somehow we don't we fail out
-        guard let imageData = data,
-            image = UIImage(data: imageData) else {
-                print("Could not get valid image data for comic: \(comic)")
-                cancel()
-                return
+        //  if it's in the cache we needn't try anything else
+        if retrieveFromCache() {
+            return
         }
         
-        self.coverImage = image
+        //  if the user is logged in to Dropbox…
+        if let client = Dropbox.authorizedClient {
+            fetchFromDropbox(withClient: client) { coverImage in
+                //  if the cover image existed in Dropbox we use it
+                if let coverImage = coverImage {
+                    //  store it and cache it
+                    self.coverImage = coverImage
+                    do {
+                        try ComicCoverImageCache.cacheCoverImage(coverImage, forComic: self.comic)
+                    } catch {
+                        print("Failed to cache the image: \(coverImage)")
+                    }
+                    return
+                    //  otherwise, if it wasn't in Dropbox we fetch it from the server
+                } else {
+                    self.fetchFromServer()
+                }
+            }
+            //  if the user is not logged in to Dropbox we know to just fetch it from the server
+        } else {
+            fetchFromServer()
+        }
     }
 }
