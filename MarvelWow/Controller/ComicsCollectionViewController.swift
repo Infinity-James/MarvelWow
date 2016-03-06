2e10f16183095ff8dadb77fed412264e9368b713
James Valaitis <james@infinityjames.com>
2016-03-06 13:57:45 +0000

    Gives the user the choice to take a new photo or choose an existing one.
    When the user selects a new photo we give the user the chance to save it
    to Dropbox.
    
    If the user is logged in to Dropbox we save it with the filename of the
    comic ID, meaning that we can use it again in the future.

--- MarvelWow/Controller/ComicsCollectionViewController.swift
+++ MarvelWow/Controller/ComicsCollectionViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 &Beyond. All rights reserved.
 //
 
+import MobileCoreServices
 import SwiftyDropbox
 import UIKit
 
@@ -29,6 +30,8 @@ class ComicsCollectionViewController: UICollectionViewController {
     private var comics = [MarvelComic]()
     /// The client we will use to fetch the comics.
     private let marvelAPIClient = MarvelAPIClient()
+    /// The comic whose cover we are currently changing (if one exists).
+    private var comicForCoverChange: MarvelComic?
     
     //	MARK: Comic Fetching
     
@@ -60,13 +63,19 @@ class ComicsCollectionViewController: UICollectionViewController {
     
     //	MARK: Comic Cover Changing
     
-    private func changeCoverForComicAtIndexPath(comicIndexPath: NSIndexPath) {
-        let alert = UIAlertController(title: "Where's the photo?", message: nil, preferredStyle: .ActionSheet)
+    private func allowUserToSelectComicCover() {
+        let actionSheet = UIAlertController(title: "Where's the photo?", message: nil, preferredStyle: .ActionSheet)
         let takePhotoAction = UIAlertAction(title: "Take a New Photo", style: .Default) { _ in
-            
+            NSOperationQueue.mainQueue().addOperationWithBlock { self.displayPhotoPickerWithSourceType(.Camera) }
         }
-        let pickPhotoAction = UIAlertAction(title: "In My Photos Library", style: .Cancel) { _ in
+        let pickPhotoAction = UIAlertAction(title: "In My Photos Library", style: .Default) { _ in
+            NSOperationQueue.mainQueue().addOperationWithBlock { self.displayPhotoPickerWithSourceType(.PhotoLibrary) }
         }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        actionSheet.addAction(takePhotoAction)
+        actionSheet.addAction(pickPhotoAction)
+        actionSheet.addAction(cancelAction)
+        presentViewController(actionSheet, animated: true, completion: nil)
     }
     
     private func displayPhotoPickerWithSourceType(sourceType: UIImagePickerControllerSourceType) {
@@ -75,12 +84,32 @@ class ComicsCollectionViewController: UICollectionViewController {
         guard UIImagePickerController.isSourceTypeAvailable(sourceType) else { return }
         
         let imagePicker = UIImagePickerController()
+        imagePicker.mediaTypes = [String(kUTTypeImage)]
         imagePicker.sourceType = sourceType
         imagePicker.delegate = self
         presentViewController(imagePicker, animated: true, completion: nil)
     }
     
-    private func saveComicCoverToDropbox() {
+    private func saveComicCoverToDropbox(coverImage: UIImage, forComic comic: MarvelComic) {
+        
+        //  if the user isn't logged in to dropbox we need them to log in first
+        guard let client = Dropbox.authorizedClient else {
+            let alert = UIAlertController(title: "Save Your Cover!", message: "If you log in to Dropbox you can save your new cover so it's always there.", preferredStyle: .Alert)
+            let logInAction = UIAlertAction(title: "Log In", style: .Default) { [unowned self] _ in Dropbox.authorizeFromController(self) }
+            let cancelAction = UIAlertAction(title: "Nope", style: .Cancel, handler: nil)
+            alert.addAction(logInAction)
+            alert.addAction(cancelAction)
+            presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        //  now that the user is logged in to Dropbox we can upload their photo
+        let imageData = UIImageJPEGRepresentation(coverImage, 0.9)
+        //  create the file name from the ID of the comic (allowing us to use it in the future for this comic)
+        let fileName = "/" + String(comic.ID) + ".jpg"
+        client.files.upload(path: fileName, body: imageData!).response { response, error in
+            //  not using the response yet
+        }
     }
     
     //	MARK: View Lifecycle
@@ -119,12 +148,17 @@ extension ComicsCollectionViewController {
     override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         collectionView.deselectItemAtIndexPath(indexPath, animated: true)
         
+        //  the collection view may update between the user selecting the comic and choosing a cover
+        //  therefore we store which comic we are changing, rather than the place in the collection view (index path)
+        comicForCoverChange = comics[indexPath.item]
+        
         //  when the user selects a comic we will give them the option to change the cover image for that comic
         let alert = UIAlertController(title: "Add Your Own Cover!", message: "Do you want to change the cover of this comic to something even more awesome?", preferredStyle: .Alert)
-        let hellYeahAction = UIAlertAction(title: "Hell Yeah", style: .Default) { [unowned self] _ in Dropbox.authorizeFromController(self) }
+        let hellYeahAction = UIAlertAction(title: "Hell Yeah", style: .Default) { [unowned self] _ in self.allowUserToSelectComicCover() }
         let boringAction = UIAlertAction(title: "Nah", style: .Cancel, handler: nil)
         alert.addAction(hellYeahAction)
         alert.addAction(boringAction)
+        presentViewController(alert, animated: true, completion: nil)
     }
 }
 
@@ -132,4 +166,18 @@ extension ComicsCollectionViewController {
 
 extension ComicsCollectionViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        guard let comicForCoverChange = comicForCoverChange else {
+            print("The user chose a new cover image, but apparently there is no comic for which we need to use this image.")
+            return
+        }
+        
+        saveComicCoverToDropbox(image, forComic: comicForCoverChange)
+        
+        self.comicForCoverChange = nil
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        comicForCoverChange = nil
+    }
 }
